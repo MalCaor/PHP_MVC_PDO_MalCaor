072c23791646731a1a0a33598f7394eda96e5064
MalCaor <xav.lemen@orange.fr>
2019-11-13 17:22:30 +0100

    verif

--- js/connection.js
+++ js/connection.js
@@ -1,5 +1,23 @@
 $( document ).ready(function() {
-$("#myModal").modal();
+  $("#myModal").modal();
+  $("#login").validate({
+	  rules: {
+	    id: {required: true},
+	    mp: {required: true},
+	    rblogin: {required: true}
+	  },
+	  messages: {
+	    id: {
+		      required: "Vous devez saisir un identifiant valide !"
+		    },
+	    mp: {
+	      required: "Vous devez saisir un mot de passe valide"
+	    },
+	    rblogin: {
+		      required: "Vous devez choisir une option"
+		    }
+	  }
+});
 });
 $("#login").submit(function( e ){
 	e.preventDefault();
