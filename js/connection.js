e35a6a576c70b7053709b0e09255a7e4435ae1b6
MalCaor <xav.lemen@orange.fr>
2019-11-15 10:47:53 +0100

    verif de log

--- js/connection.js
+++ js/connection.js
@@ -2,24 +2,33 @@ $( document ).ready(function() {
   $("#myModal").modal();
   $("#login").validate({
 	  rules: {
-	    id: {required: true},
-	    mp: {required: true},
+	    id: {required: true, minlength: 2},
+	    mp: {required: true, minlength: 2},
 	    rblogin: {required: true}
 	  },
 	  messages: {
 	    id: {
-		      required: "Vous devez saisir un identifiant valide !"
+		      required: "Vous devez saisir un identifiant valide !",
+          minlength: "Pas asser long long long..."
 		    },
 	    mp: {
-	      required: "Vous devez saisir un mot de passe valide"
+	      required: "Vous devez saisir un mot de passe valide !",
+        minlength: "Pas asser long long long..."
 	    },
 	    rblogin: {
-		      required: "Vous devez choisir une option"
+		      required: "Vous devez choisir une option !"
 		    }
 	  }
-});
-});
-$("#login").submit(function( e ){
-	e.preventDefault();
-	alert("submit");
+  });
+  $("#login").submit(function( e ){
+    e.preventDefault();
+    console.log("Je suis log");
+    alert("submit");
+    if($("#login").valid()){
+      alert("ok");
+    }
+    else {
+      alert("non");
+    }
+  });
 });
